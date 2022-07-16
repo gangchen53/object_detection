d1d49ed5555814a7239375158c889e9277aada83
Gang Chen <89380407+gangchen53@users.noreply.github.com>
2022-07-16 09:37:47 +0800

    add clip_video function

--- utils.py
+++ utils.py
@@ -40,3 +40,47 @@ def video2images(video_path: str,
             cv2.imwrite(str(img_save_path), frame)
         current_frame_id += 1
     tbar.close()
+
+
+def clip_video(video_path: str,
+               video_save_dir: str,
+               start_second: int,
+               end_second: int,
+               ):
+    video_path = Path(video_path)
+    video_save_dir = Path(video_save_dir)
+    video_save_dir.mkdir(exist_ok=True, parents=True)
+
+    cap = cv2.VideoCapture(str(video_path))
+    if not cap.isOpened():
+        print('Cannot open camera!')
+        exit()
+
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    start_frame = start_second * fps
+    end_frame = end_second * fps
+
+    # If the suffix of the video file is avi, then the encoding format is MJPG.
+    if 'avi' in video_path.suffix:
+        fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+    else:
+        raise NotImplementedError
+    video_save_path = video_save_dir / video_path.name
+    video_writer = cv2.VideoWriter(str(video_save_path), fourcc, fps, (frame_width, frame_height))
+
+    frame_counter = 1
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print('Cannot receive frame (stream end?). Exiting ...\n')
+            break
+
+        if start_frame <= frame_counter <= end_frame:
+            video_writer.write(frame)
+        elif frame_counter > end_frame:
+            break
+
+        frame_counter += 1
